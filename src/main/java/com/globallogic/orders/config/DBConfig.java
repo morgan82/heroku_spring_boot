8b663de5c5221bcd793263e957b565fa478310aa
leonardo.morganti <leonardo.morganti@globallogic.com>
2016-02-03 16:28:13 -0300

    postgre  connection  fix 3

--- src/main/java/com/globallogic/orders/config/DBConfig.java
+++ src/main/java/com/globallogic/orders/config/DBConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 
 /**
  * Created by lmorganti on 01/02/16.
@@ -14,42 +15,25 @@ import javax.sql.DataSource;
 @Slf4j
 @Configuration
 public class DBConfig {
+    //    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return new org.apache.tomcat.jdbc.pool.DataSource();
+//    }
     @Bean
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
+    public DataSource dataSource() throws URISyntaxException {
+        String url = System.getProperty("database.url");
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        log.info("***********************************************");
+        log.info("dbProperty : " + url);
+        if (url != null) {
+            dataSource.setUrl(url);
+        }
+        log.info("////////////////////////////////////////////////////");
+        return dataSource;
     }
-//
-//    @Value("${datasource.mine.url}")
-//    private String url;
-//    @Value("${datasource.mine.username}")
-//    private String username;
-//    @Value("${datasource.mine.password}")
-//    private String password;
-//
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource postgreDataSource() throws URISyntaxException {
-//        String dbProperty = System.getProperty("database.url");
-//        URI dbUri;
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        if (dbProperty != null) {
-//            dbUri = new URI(dbProperty);
-//            basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
-//            basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
-//            basicDataSource.setUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
-//
-//        }else {
-//            basicDataSource.setUsername(username);
-//            basicDataSource.setPassword(password);
-//            basicDataSource.setUrl(url);
-//        }
-//        log.info("********************************************");
-//        log.info("username :"+basicDataSource.getUsername());
-//        log.info("password :"+basicDataSource.getPassword());
-//        log.info("url :"+basicDataSource.getUrl());
-//        return basicDataSource;
-//    }
 
 }
