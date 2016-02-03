373177244736f5780ee958747682720beb070515
leonardo.morganti <leonardo.morganti@globallogic.com>
2016-02-03 15:10:50 -0300

    manifest fix bug 2

--- src/main/java/com/globallogic/orders/config/DBConfig.java
+++ src/main/java/com/globallogic/orders/config/DBConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 import java.net.URI;
@@ -20,36 +21,50 @@ import java.util.logging.Logger;
 @Slf4j
 @Configuration
 public class DBConfig {
-
-    @Value("${datasource.mine.url}")
-    private String url;
-    @Value("${datasource.mine.username}")
-    private String username;
-    @Value("${datasource.mine.password}")
-    private String password;
-
     @Bean
+    @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource postgreDataSource() throws URISyntaxException {
+    public DataSource dataSource() throws URISyntaxException {
         String dbProperty = System.getProperty("database.url");
-        URI dbUri;
-        BasicDataSource basicDataSource = new BasicDataSource();
+        DataSourceBuilder builder = DataSourceBuilder.create();
         if (dbProperty != null) {
-            dbUri = new URI(dbProperty);
-            basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
-            basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
-            basicDataSource.setUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
-
-        }else {
-            basicDataSource.setUsername(username);
-            basicDataSource.setPassword(password);
-            basicDataSource.setUrl(url);
+            URI dbUri = new URI(dbProperty);
+            builder.username(dbUri.getUserInfo().split(":")[0]);
+            builder.password(dbUri.getUserInfo().split(":")[1]);
+            builder.url("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
         }
-        log.info("********************************************");
-        log.info("username :"+basicDataSource.getUsername());
-        log.info("password :"+basicDataSource.getPassword());
-        log.info("url :"+basicDataSource.getUrl());
-        return basicDataSource;
+        return builder.build();
     }
+//
+//    @Value("${datasource.mine.url}")
+//    private String url;
+//    @Value("${datasource.mine.username}")
+//    private String username;
+//    @Value("${datasource.mine.password}")
+//    private String password;
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource postgreDataSource() throws URISyntaxException {
+//        String dbProperty = System.getProperty("database.url");
+//        URI dbUri;
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        if (dbProperty != null) {
+//            dbUri = new URI(dbProperty);
+//            basicDataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
+//            basicDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
+//            basicDataSource.setUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
+//
+//        }else {
+//            basicDataSource.setUsername(username);
+//            basicDataSource.setPassword(password);
+//            basicDataSource.setUrl(url);
+//        }
+//        log.info("********************************************");
+//        log.info("username :"+basicDataSource.getUsername());
+//        log.info("password :"+basicDataSource.getPassword());
+//        log.info("url :"+basicDataSource.getUrl());
+//        return basicDataSource;
+//    }
 
 }
