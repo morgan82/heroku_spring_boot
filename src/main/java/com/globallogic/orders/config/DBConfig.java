37e4e7dfd005fdd9060f5cdd7d8da232b8b2fd2b
leonardo.morganti <leonardo.morganti@globallogic.com>
2016-02-04 09:43:19 -0300

    test without datasource bean

--- src/main/java/com/globallogic/orders/config/DBConfig.java
+++ src/main/java/com/globallogic/orders/config/DBConfig.java
@@ -1,12 +1,7 @@
 package com.globallogic.orders.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 /**
  * Created by lmorganti on 01/02/16.
@@ -14,11 +9,11 @@ import javax.sql.DataSource;
 @Slf4j
 @Configuration
 public class DBConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
-    }
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return new org.apache.tomcat.jdbc.pool.DataSource();
+//    }
 
 }
